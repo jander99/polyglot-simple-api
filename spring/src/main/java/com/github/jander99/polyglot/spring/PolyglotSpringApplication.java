38545b8b0eb0459cf7e1416f907d4ce877de4a80
Jeff Anderson <jander99@gmail.com>
2020-06-27 19:47:54 -0400

    Spring Project

---
+++ spring/src/main/java/com/github/jander99/polyglot/spring/PolyglotSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.jander99.polyglot.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyglotSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolyglotSpringApplication.class, args);
+    }
+
+}
---
+++ spring/src/test/java/com/github/jander99/polyglot/spring/PolyglotSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jander99.polyglot.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyglotSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
